fabb6e3106b7f875d282cccc853315204ab2ef96
PhongDiep <phongdiep0712@gmail.com>
2024-04-05 17:21:58 -0700

    Created css and components folders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import index from './pages/index'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Dashboard from '../../frontend/src/pages/Dashboard.jsx'
 function App() {
   return (
     <BrowserRouter>
