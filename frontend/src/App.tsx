dd5e846c16510e7c8734e14e655b6cb4c92e2d4f
Hasnain Mucklai <mucklaihasnain@gmail.com>
2024-04-06 17:20:47 -0700

    Added about us and logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,32 @@
-
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { AuthProvider } from './contexts/authContext/authContext';
 import HomePage from './pages/Login';
-import SignUp from './pages/SignUp'
-import Inbox from './pages/Inbox'
-import Dashboard from './pages/Dashboard'
-import MyListing from '../src/pages/MyListing'
+import SignUp from './pages/SignUp';
+import Inbox from './pages/Inbox';
+import Dashboard from './pages/Dashboard';
+import MyListing from './pages/MyListing'; 
 import RentOutForm from './pages/RentOutForm';
 import Layout from './components/Layout';
+import AboutUs from './pages/AboutUs'; 
+
 function App() {
   return (
     <AuthProvider>
-       <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<HomePage />}/>
-          <Route path="/signup" element={<SignUp />}/>
-          <Route path="/" element={<Layout/>}>
+          <Route path="/login" element={<HomePage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Layout />}>
             <Route index element={<Dashboard />} />
-            <Route path="/inbox" element={<Inbox />} />
-            <Route path="/mylistings" element={<MyListing/>} />
-            <Route path="/rentout" element={<RentOutForm/>} />
+            <Route path="inbox" element={<Inbox />} />
+            <Route path="mylistings" element={<MyListing />} />
+            <Route path="rentout" element={<RentOutForm />} />
+            <Route path="about" element={<AboutUs />} /> {/* Add this line */}
           </Route>
         </Routes>
-      </BrowserRouter> 
+      </BrowserRouter>
     </AuthProvider>
   );
 }
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import  '../css/layout.css'
-import { FaHouse } from "react-icons/fa6";
-import { doSignOut } from '../firebase/auth'
+import React from 'react';
+import '../css/layout.css';
+import { FaUser } from "react-icons/fa"; 
+import { doSignOut } from '../firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
+
+// Example logo import - adjust the path as necessary
+import logoImage from '../assets/HouseShare logo.png'; 
 
 const Layout = () => {
   const navigate = useNavigate();
@@ -11,20 +14,18 @@ const Layout = () => {
     <div className='container'>
         <div className='tab_bar'>
           <div className='tabBarLeftSide'>
-            <FaHouse size={30} color='#444B6E'/>
+          <FaUser size={30} color='#444B6E'/>
+            {/* Logo image added here */}
+            <img src={logoImage} alt="Logo" className="logo_image"/>
             <p className='dashboard_app_name'>House Share</p>
           </div>
           <div>
             <button className='tabBarButton' onClick={() => navigate("/")}>Dashboard</button>
-            <button className='tabBarButton' onClick={() => {
-                navigate("/inbox");
-              }} >Inbox</button>
-            <button className='tabBarButton'  onClick={() => navigate("/rentout")} >Rent out</button>
+            <button className='tabBarButton' onClick={() => navigate("/inbox")}>Inbox</button>
+            <button className='tabBarButton' onClick={() => navigate("/rentout")}>Rent out</button>
             <button className='tabBarButton' onClick={() => navigate("/mylistings")}>My listings</button>
-            <button className='tabBarButton' onClick={() => { 
-                doSignOut();
-                navigate("/login");
-              } }>Sign out</button>
+            <button className='tabBarButton' onClick={() => navigate("/about")}>About Us</button>
+            <button className='tabBarButton' onClick={() => { doSignOut(); navigate("/login"); }}>Sign out</button>
           </div>
         </div>
         <Outlet/>
@@ -32,4 +33,4 @@ const Layout = () => {
   )
 }
 
-export default Layout
+export default Layout;
